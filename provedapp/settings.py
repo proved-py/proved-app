6f046cf8e85889c69bed427127cf0f640e9ee468
Marco Pegoraro <pegoraro@pads.rwth-aachen.de>
2021-01-20 07:42:10 +0100

    Added apps to settings.

--- provedapp/settings.py
+++ provedapp/settings.py
@@ -118,3 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Additional non-default applications
+INSTALLED_APPS += [
+    'apps.explorer',
+    'apps.selector',
+]
