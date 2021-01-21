3f5d074e6aba4d9f4160ce238dfbc2357d20cda7
Marco Pegoraro <pegoraro@pads.rwth-aachen.de>
2021-01-21 18:56:51 +0100

    Created test view for the dashboard.

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+APP_BASE_DIR = 'dashboard/'
+APP_TEMPLATES_DIR = APP_BASE_DIR + 'templates/'
+
+
+def dashboard(request):
+    context = {'hello_world': 'Hello, world!'}
+    return render(request, APP_TEMPLATES_DIR + 'dashboard.html', context)
