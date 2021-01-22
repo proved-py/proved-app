a235e16648fddfc51ef64d3adb1326a90e1aaa2e
Marco Pegoraro <pegoraro@pads.rwth-aachen.de>
2021-01-22 10:16:54 +0100

    Added base templates directories.

--- provedapp/settings.py
+++ provedapp/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'provedapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['provedapp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
