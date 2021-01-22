e908fdde6cb2186083a0583cda6295c0e595b047
Marco Pegoraro <pegoraro@pads.rwth-aachen.de>
2021-01-22 10:04:22 +0100

    Fixed template references in view.

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 
-APP_BASE_DIR = 'dashboard/'
-APP_TEMPLATES_DIR = APP_BASE_DIR + 'templates/'
-
 
 def dashboard(request):
     context = {'hello_world': 'Hello, world!'}
-    return render(request, APP_TEMPLATES_DIR + 'dashboard.html', context)
+    return render(request, 'dashboard.html', context)
