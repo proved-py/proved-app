78bffaf188d380a91c6f95941450d0a0e18ec4cc
Marco Pegoraro <pegoraro@pads.rwth-aachen.de>
2021-01-22 19:22:05 +0100

    Configured media files.

--- provedapp/settings.py
+++ provedapp/settings.py
@@ -128,3 +128,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+
+# Media files
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
--- provedapp/urls.py
+++ provedapp/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 # from django.contrib import admin
 
 urlpatterns = [
-    path('', include('apps.dashboard.urls'))
+    path('', include('apps.dashboard.urls')),
     # path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
