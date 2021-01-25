39d4c6ac0c270e6ee72275d9895d5d5a72a6f18b
Marco Pegoraro <pegoraro@pads.rwth-aachen.de>
2021-01-25 15:22:47 +0100

    Created stub for the selector view.

--- apps/selector/views.py
+++ apps/selector/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def artifact_selector(type):
+    artifact_map = {}
+
+
+def selector(request):
+    if request.method == 'POST':
+        if 'algorithm_request' in request.method:
+            pass
+        elif 'upload' in request.method:
+            pass
+        elif 'delete' in request.method:
+            pass
+        else: # 'algorithm' is in request.POST, dispatch to the correct algorithm page
+            return render(request, request.POST['algorithm'], )
+
+    context = {}
+    return render(request, 'selector.html', context)
