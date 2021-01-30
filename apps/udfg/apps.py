1df4aa8a647c626f39330d0d549c4f395db62d52
Marco Pegoraro <pegoraro@pads.rwth-aachen.de>
2021-01-30 21:40:44 +0100

    Created udfg app.

---
+++ apps/udfg/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class UdfgConfig(AppConfig):
+    name = 'udfg'
---
+++ apps/udfg/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.udfg, name='udfg'),
+]
